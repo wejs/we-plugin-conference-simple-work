d44662b030f734f0bccdb80d60e7461c3584e6e6
albertosouza <alberto.souza.99@gmail.com>
2015-09-18 08:56:50 -0300

    add we-cf-cfwork-btn helper and template

---
+++ server/helpers/we-cf-cfwork-btn.js
@@ -0,0 +1,32 @@
+/**
+ * Helper to render cfwork registration
+ *
+ * usage:  {{we-cf-cfwork-btn event=event}}
+ */
+module.exports = function(we) {
+  return function cfWorkBTNHelper(opts) {
+    if (
+      !opts.hash.event ||
+      opts.hash.event.sendWorkStatus != 'open'
+    ) {
+      return '';
+    }
+
+    var __ = (opts.hash.__ || we.i18n.__);
+
+    var ctx = {
+      btnClass: (opts.hash.class || ' btn btn-default ') + ' btn-cfwork ',
+      href: '/login?redirectTo=/event/'+opts.hash.event.id,
+      text: __('cfwork.btn.text')
+    };
+
+    if (opts.hash.userId)
+      ctx.href =  '/event/'+event.id+'/user/'+opts.hash.userId+'/cfwork';
+
+    return new we.hbs.SafeString(we.view.renderTemplate(
+      'cfwork/we-cf-cfwork-btn',
+      (opts.hash.theme || this.theme),
+      ctx
+    ));
+  }
+}
